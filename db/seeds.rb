c6ff566ca76d5e71ed696fc246110f772e7d19fa
Bayburin R.F <ravil@ravil.npopm.ru>
2016-10-07 15:22:51 +0700

    Changed title in Clusters page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,3 @@ Role.create(name: 'manage_serv')
 
 user = User.create(username: 'admin', password: 'admin')
 user.add_role :admin
-
-user = User.create(username: 'test', password: 'test')
-user.add_role :manage_serv
