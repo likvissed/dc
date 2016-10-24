4cd5e945e99c670d0380a7b0da81f781d61a1c59
Bayburin R.F <bayburin@iss-reshetnev.ru>
2016-10-24 11:23:04 +0700

    Fixed service_controller.rb. Changed string type to symbol.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -58,37 +58,37 @@ class ServicesController < ApplicationController
           },
           except: [:created_at, :updated_at]).each do |s|
             # Привести "Режим функционирования" к сокращенному формату
-            s['time_work']  = short_time_work(s['time_work'])
+            s[:time_work]  = short_time_work(s['time_work'])
 
             # Флаги
-            s['flags'] = {}
+            s[:flags] = {}
             # Приоритет функционирования сервиса
-            s['flags']['priority'] = s['priority']
+            s[:flags][:priority] = s['priority']
             # Для тестовых затач проверить дату дедлайна (true - если now > deadline)
-            s['flags']['deadline'] = if s['deadline'].nil?
-                                       false
-                                     else
-                                       now > s['deadline']
-                                     end
+            s[:flags][:deadline] = if s['deadline'].nil?
+                                     false
+                                   else
+                                     now > s['deadline']
+                                   end
 
             # Передать только фамилию у ответственных
             if !s.include?('contact_1') && !s.include?('contact_2')   # Если нет контактов
-              s['contacts'] = nil
+              s[:contacts] = nil
             elsif !s.include?('contact_1') && s.include?('contact_2') # Если есть только контакт 2
-              s['contacts'] = s['contact_2']['info'].split(' ')[0]
+              s[:contacts] = s['contact_2']['info'].split(' ')[0]
             elsif !s.include?('contact_2')                            # Если есть только контакт 1
-              s['contacts'] = s['contact_1']['info'].split(' ')[0]
+              s[:contacts] = s['contact_1']['info'].split(' ')[0]
             else                                                      # Если оба контакта
-              s['contacts'] = s['contact_1']['info'].split(' ')[0] + ', ' + s['contact_2']['info'].split(' ')[0]
+              s[:contacts] = s['contact_1']['info'].split(' ')[0] + ', ' + s['contact_2']['info'].split(' ')[0]
             end
 
             s.delete('contact_1_id')
             s.delete('contact_2_id')
 
-            s['scan']       = ''
-            s['act']        = ''
-            s['instr_rec']  = ''
-            s['instr_off']  = ''
+            s[:scan]      = ''
+            s[:act]       = ''
+            s[:instr_rec] = ''
+            s[:instr_off] = ''
           end
         render json: data
       end
@@ -104,7 +104,7 @@ class ServicesController < ApplicationController
         service = @service.as_json(except: [:contact_1_id, :contact_2_id, :created_at, :updated_at])
 
         # Установить номер формуляра
-        service['number'] = @service.get_service_number
+        service[:number] = @service.get_service_number
         # Проверить, прошел ли дедлайн для тестового сервиса
         deadline = if service['deadline'].nil?
                      false
@@ -112,7 +112,7 @@ class ServicesController < ApplicationController
                      Time.now.to_date > service['deadline']
                    end
         # Установить дедлайн для приоритета "Тестирование и отладка"
-        service['deadline'] = I18n.l(@service.deadline, format: :long) unless @service.deadline.nil?
+        service[:deadline] = I18n.l(@service.deadline, format: :long) unless @service.deadline.nil?
 
         render json: {
           service:      service,
