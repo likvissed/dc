6de4ca1966b87bab8122798b1de527034f1e90fa
Bayburin R.F <bayburin@iss-reshetnev.ru>
2016-11-21 16:52:35 +0700

    Changed web_console settings.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Web console settings
+  config.web_console.whitelisted_ips = '10.1.8.26'
 end
